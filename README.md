0391b11bb97cad0428c7f8b992852e41164263b5
Oskar Straszynski <oskar.straszynski@gmail.com>
2023-11-10 23:33:24 +0100

    touches

---
+++ README.md
@@ -0,0 +1,26 @@
+# Java Text-Based RPG Game
+
+This repository contains a Java-based text RPG game, offering a rich and interactive command-line experience. Players can engage in battles, buy and sell items, and explore different locations within the game.
+
+## Features
+
+- **Character Creation**: Players can create and customize their characters.
+- **Combat System**: Engage in battles with opponents in the Arena with options for manual and auto-combat.
+- **Inventory Management**: Players can buy, sell, and use items like swords, armor, and health potions.
+- **Dynamic Opponents**: Opponents in the Arena get stronger or weaker based on player performance.
+- **Shop**: A shop where players can purchase various items, including some unique and humorous ones.
+
+## How to Run
+
+To run the game, ensure you have Java installed on your system. Clone the repository, navigate to the source directory, and compile and run the Main class:
+
+```bash
+javac Main.java
+java Main
+```
+## Game Instructions
+- Start the Game: Choose to start a new game or load an existing one.
+- Character Management: Customize your character and manage your inventory.
+- Explore Locations: Visit different locations like the Arena, Shop, and Training Camp.
+- Engage in Battles: Fight opponents in the Arena and use strategies to win.
+- Buy and Sell Items: Visit the shop to buy new items or sell your existing items.
--- src/characters/Player.java
+++ src/characters/Player.java
@@ -1,5 +1,6 @@
 package characters;
 
+import items.Armor;
 import items.HealthPotion;
 import items.Item;
 import items.Sword;
@@ -29,10 +30,19 @@ public class Player extends Character {
             writer.write(getMaxHealth() + "\n");
             writer.write(getStrength() + "\n");
             writer.write(getGold() + "\n");
+            writer.write(victories + "\n");
+            writer.write(defeats + "\n");
+
+            // Save inventory items
+            for (Item item : inventory) {
+                writer.write(item.getName() + ",");
+            }
+            writer.write("\n");
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
 
     public static Player loadStatsFromFile(String filename) {
         try (BufferedReader reader = new BufferedReader(new FileReader(filename))) {
@@ -41,12 +51,45 @@ public class Player extends Character {
             int maxHealth = Integer.parseInt(reader.readLine());
             int strength = Integer.parseInt(reader.readLine());
             int gold = Integer.parseInt(reader.readLine());
-            return new Player(name, health, maxHealth, strength, gold);
+            int victories = Integer.parseInt(reader.readLine());
+            int defeats = Integer.parseInt(reader.readLine());
+
+            Player player = new Player(name, health, maxHealth, strength, gold);
+            player.victories = victories;
+            player.defeats = defeats;
+
+            // Load inventory items
+            String[] items = reader.readLine().split(",");
+            for (String itemName : items) {
+                if (!itemName.isEmpty()) {
+                    // Assuming a method to create items based on name
+                    Item item = createItemByName(itemName);
+                    player.addItemToInventory(item);
+                }
+            }
+
+            return player;
         } catch (IOException e) {
             e.printStackTrace();
             return null;
         }
     }
+    private Item createItemByName(String name) {
+        return switch (name) {
+            case "Basic Sword" -> new Sword("Basic Sword", 100, 10);
+            case "Basic Sandals" -> new Armor("Basic Sandals", 150, 5);
+            case "Health Potion" -> new HealthPotion("Health Potion", 10, 10);
+            case "Rubber Chicken Sword" -> new Sword("Rubber Chicken Sword", 200, 5);
+            case "Not Sandals" -> new Armor("Not Sandals", 300, 0);
+            case "Tickling Feather" -> new Sword("Tickling Feather", 50, -5);
+            case "Cursed Coin" -> new Item("Cursed Coin", 200);
+            case "Portable Black Hole" -> new Sword("Portable Black Hole", 500, 100);
+            case "Golden Sandals" -> new Armor("Golden Sandals", 350, 50);
+            default -> null;
+        };
+    }
+
+
     public void addItemToInventory(Item item) {
         inventory.add(item);
     }
